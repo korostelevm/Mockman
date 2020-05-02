72033e35842339c510fea4445ecc9fff78f623e1
korostelevm <korostelevm@gmail.com>
2020-05-02 10:30:59 -0400

    asdf

---
+++ vue/src/components/Service/EditMock.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+    <b-button size="sm" v-b-modal.modal-1>Create a mock</b-button>
+    <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">Hello from modal!</p>
+    </b-modal>
+</div>
+</template>
+
+<script>
+export default {
+    props:['method','value'],
+    data() {
+      return {
+        loading: null,
+      }
+    },
+    mounted: function() {
+    },
+    created: function() {
+    },
+    methods: {
+      }
+  }
+</script>
+
+<style scoped>
+</style>

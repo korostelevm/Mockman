e1f53f9fe1e5b4fa3545b481e0e2a971a7e42dcf
korostelevm <korostelevm@gmail.com>
2020-05-17 11:45:17 -0400

    cassandra

--- service/models/models.js
+++ service/models/models.js
@@ -1,12 +1,10 @@
 var mocks = require('./mocks')
 var services = require('./services')
 var routes = require('./routes')
-var cassandra = require('./cassandra')
 
 
 module.exports= {
     mocks,
     routes,
     services,
-    cassandra
 }
